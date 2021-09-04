bb7542362bb4c44e005ce1eff869f93129af41cd
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-04 20:38:01 +0700

    Menambah LoginViewModel dan AkunController

---
+++ DepotSneakers/Controllers/AkunController.cs
@@ -0,0 +1,23 @@
+﻿using DepotSneakers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepotSneakers.Controllers
+{
+    public class AkunController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AkunController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {            
+            return View();
+        }
+    }
+}
