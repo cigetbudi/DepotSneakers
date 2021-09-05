9951fd28bb37f0a82fa862e3e9a076cf0116c148
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-05 18:02:10 +0700

    Menambah Idenityuser table dan register POST

--- DepotSneakers/Controllers/AkunController.cs
+++ DepotSneakers/Controllers/AkunController.cs
@@ -1,4 +1,6 @@
 ﻿using DepotSneakers.Models;
+using DepotSneakers.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,16 @@ namespace DepotSneakers.Controllers
     public class AkunController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public AkunController(ApplicationDbContext db)
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AkunController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Login()
@@ -22,6 +31,27 @@ namespace DepotSneakers.Controllers
 
         public IActionResult Daftar()
         {
+            return PartialView();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Daftar(DaftarViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return PartialView();
         }
     }
--- DepotSneakers/Migrations/20210905105908_addregister.Designer.cs
+++ DepotSneakers/Migrations/20210905105908_addregister.Designer.cs
@@ -4,14 +4,16 @@ using DepotSneakers.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DepotSneakers.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210905105908_addregister")]
+    partial class addregister
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
